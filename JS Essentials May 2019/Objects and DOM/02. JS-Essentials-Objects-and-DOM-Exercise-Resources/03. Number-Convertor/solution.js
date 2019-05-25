0f19e2686ccf745144a0dba19a9a1d6311c1b44f
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-05-25 15:49:24 +0300

    Add p03_NumberConvertor solution

--- JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/03. Number-Convertor/solution.js
+++ JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/03. Number-Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    let inputElement = document.getElementById('input');
+    let selectElementTo = document.getElementById('selectMenuTo');
+    let outputElement = document.getElementById('result');
+    let convertButton = document.getElementsByTagName('button')[0];
 
-    //TODO...
+    let binaryOption = document.createElement('option');
+    binaryOption.text = "Binary";
+    binaryOption.value = 'binary';
+    selectElementTo.add(binaryOption);
+
+    let hexOption = document.createElement('option');
+    hexOption.text="Hexadecimal";
+    hexOption.value = "hexadecimal";
+    selectElementTo.add(hexOption);
+
+
+    convertButton.addEventListener('click', (e)=>{
+        let valueForConvert = Number(inputElement.value);
+        let convertOption = selectElementTo.value;
+        if(convertOption === "binary"){
+            outputElement.value = valueForConvert.toString(2);
+        }else if(convertOption === "hexadecimal"){
+            outputElement.value = valueForConvert.toString(16).toUpperCase();
+        }
+    });
 }
