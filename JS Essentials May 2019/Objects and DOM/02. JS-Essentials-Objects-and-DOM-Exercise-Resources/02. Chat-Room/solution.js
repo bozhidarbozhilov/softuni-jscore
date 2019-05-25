f3ef4498c2dd4e57d04993c1031b9172a9af1190
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-05-25 09:38:28 +0300

    Add p02_ChatRoom solution

--- JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/02. Chat-Room/solution.js
+++ JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/02. Chat-Room/solution.js
@@ -1,5 +1,17 @@
 function solve() {
-   //TODO...
+   let sendButton = document.getElementById('send');
+   let textAreaElement = document.getElementById('chat_input');
+   let allMessageElement = document.getElementById('chat_messages');
+
+   sendButton.addEventListener('click', ()=>{
+      let messageToSend = textAreaElement.value;
+      let divMessageClass = document.createElement('div');
+      divMessageClass.classList.add('message');
+      divMessageClass.classList.add('my-message');
+      divMessageClass.textContent = messageToSend;
+      allMessageElement.appendChild(divMessageClass);
+      textAreaElement.value = '';
+   });
 }
 
 
