ccca68d16a3349fcc3045fa8958245896e3f6c5c
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-05-25 17:56:32 +0300

    Add p04.JS-Quizz solution

--- JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/04. JavaScript-Quizz/solution.js
+++ JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/04. JavaScript-Quizz/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-  //TODO...
+  let questionElements = document.getElementsByTagName('ul');
+  let questionCounter = 0;
+  let rightAnswersCounter = 0;
+
+
+  for(let i=0;i<questionElements.length; i++){
+      let clickElements = questionElements[i].getElementsByTagName('li');
+      
+      for (const element of clickElements) {
+        element.addEventListener('click', ()=>{
+          if(i===0 && element.classList.contains('low-value')){
+              rightAnswersCounter += 1;
+          }
+          if(i===1 && element.classList.contains('high-value')){
+            rightAnswersCounter += 1;
+          }
+          if(i===2 && element.classList.contains('low-value')){
+            rightAnswersCounter += 1
+          }
+          questionElements[i].parentNode.style.display='none';
+          if(i+1===3){
+            if(rightAnswersCounter === 3){
+              questionElements[3].querySelector('h1').textContent = 'You are recognized as top JavaScript fan!';
+            }else{
+              questionElements[3].querySelector('h1').textContent = `You have ${rightAnswersCounter} right answers`;
+            }
+           questionElements[3].style.display='block';
+          }else{
+            questionElements[i+1].parentNode.style.display='block';
+          }
+          
+        });
+      }
+  }
+ 
+
 }
