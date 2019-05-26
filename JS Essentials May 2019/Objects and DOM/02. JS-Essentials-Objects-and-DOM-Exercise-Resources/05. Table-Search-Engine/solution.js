2091fa5b75e6d36f8d4fa932bf6354731a38df8f
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-05-26 10:57:48 +0300

    Add p05.TableSearchEngine solution

--- JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/05. Table-Search-Engine/solution.js
+++ JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/05. Table-Search-Engine/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
+   let searchField = document.getElementById('searchField');
+   let searchBtn = document.getElementById('searchBtn');
+   let tableBody = document.getElementsByTagName('tbody')[0];
+   let tableRows = tableBody.getElementsByTagName('tr');
+
+   searchBtn.addEventListener('click', ()=>{
+      clearSelected();
+      let searchStr = searchField.value.toUpperCase();
+      
+      for (const row of tableRows) {
+         let rowCells = row.getElementsByTagName('td');
+         for (const cell of rowCells) {
+            if(cell.textContent.toUpperCase().indexOf(searchStr)>-1){
+               row.className='select';
+            }
+         }
+      }
+      searchField.value='';
+   });
+
+   function clearSelected(){
+      for(const row of tableRows){
+         if(row.classList.contains('select')){
+            row.classList.remove('select');
+         }
+      }
+   }
 }
