91441ae0dab1f56b1efeb6bd99fe9ccfbcabca86
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-06-04 18:24:24 +0300

    Add p09_OrderTheNames solution

--- JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/09. Order-the-Names/solution.js
+++ JS Essentials May 2019/Objects and DOM/02. JS-Essentials-Objects-and-DOM-Exercise-Resources/09. Order-the-Names/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+    let inputElement = document.querySelector('article input');
+    const addButton = document.querySelector('article button');
+    let listItems = document.querySelectorAll('ol li');
 
-    //TODO...
+    addButton.addEventListener('click', ()=>{
+        let inputName = inputElement.value.charAt(0).toUpperCase() + inputElement.value.slice(1).toLowerCase();
+        let letterIndex = inputName.charCodeAt(0) - 65;
+        if(listItems[letterIndex].textContent === ''){
+            listItems[letterIndex].textContent += inputName;
+        }else{
+            listItems[letterIndex].textContent += ', '+inputName;
+        }
+        inputElement.value = '';
+    });
 }
