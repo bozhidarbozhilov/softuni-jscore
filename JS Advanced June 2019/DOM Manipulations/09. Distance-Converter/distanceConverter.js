06bceca1f67af32a76b19e5a132f7293c3354fea
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-06-24 17:03:32 +0300

    Add p09_DistanceConverter solution

--- JS Advanced June 2019/DOM Manipulations/09. Distance-Converter/distanceConverter.js
+++ JS Advanced June 2019/DOM Manipulations/09. Distance-Converter/distanceConverter.js
@@ -1,3 +1,33 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const inputDistanceElement = document.getElementById('inputDistance');
+    const inputUnitsElement = document.getElementById('inputUnits');
+
+    const outputDistanceElement = document.getElementById('outputDistance');
+    const outputUnitsElement = document.getElementById('outputUnits');
+
+    const convertBtn = document.getElementById('convert');
+
+    const toMeters = {
+        km: 1000,
+        m:1,
+        cm:0.01,
+        mm:0.001,
+        mi:1609.34,
+        yrd:0.9144,
+        ft:0.3048,
+        in:0.0254
+    }
+
+    const convertInput = ()=>{
+        let value = Number(inputDistanceElement.value);
+        let inputUnits = inputUnitsElement.value;
+
+        let valueInMeters = value * toMeters[inputUnits];
+        
+        let outputUnits = outputUnitsElement.value;
+        
+        outputDistanceElement.value = valueInMeters / toMeters[outputUnits];
+    }
+    convertBtn.addEventListener('click', convertInput);
+
 }
