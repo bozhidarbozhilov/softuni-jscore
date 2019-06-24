b239e572a11bd477511ef3304d63deb76ceab080
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-06-24 10:50:38 +0300

    Add solutions for p07.Locked Profile and p08.

--- JS Advanced June 2019/DOM Manipulations/07. Locked-Profile/app.js
+++ JS Advanced June 2019/DOM Manipulations/07. Locked-Profile/app.js
@@ -1,3 +1,37 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const SELECTORS = {
+        PROFILE_ELEMENTS: '.profile',
+        RADIO_BUTTONS: 'input[type="radio"]',
+        SHOW_BUTTON: 'button',
+        HIDDEN_FIELDS: '#user{{index}}HiddenFields'
+    }
+    const profileElements = document.querySelectorAll(SELECTORS.PROFILE_ELEMENTS);
+
+    const checkForLock = function(radios){
+        let checked = Array.from(radios).find(btn=>btn.checked);
+        return checked.value === 'lock' ? true : false;
+    }
+
+    const toggleHiddenFields = function(element,index){
+        let isLocked = checkForLock(element.querySelectorAll(SELECTORS.RADIO_BUTTONS));
+        if(!isLocked){
+            let hiddenStyleStatus = element.querySelector(SELECTORS.HIDDEN_FIELDS.replace('{{index}}', index+1));
+            let showButtonText = element.querySelector(SELECTORS.SHOW_BUTTON);
+            if(hiddenStyleStatus.style.display === 'block'){
+                hiddenStyleStatus.style.display = 'none';
+                showButtonText.textContent = 'Show more';
+            }else{
+                hiddenStyleStatus.style.display = 'block';
+                showButtonText.textContent = 'Hide it';
+            }
+             
+        }
+        
+    }
+    
+    Array.from(profileElements)
+                            .forEach((profile, index)=>{
+                                let showButtonElement = profile.querySelector(SELECTORS.SHOW_BUTTON);
+                                showButtonElement.addEventListener('click', ()=>toggleHiddenFields(profile, index));
+                            });
 }
--- JS Advanced June 2019/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
+++ JS Advanced June 2019/DOM Manipulations/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    // const divMainElements = document.querySelectorAll('#main div');
+    // const encodeAndSendEl = divMainElements[0];
+    // const decodeAndReadEl = divMainElements[1];
+    // const encodeButton = encodeAndSendEl.querySelector('button');
+    // const decodeButton = decodeAndReadEl.querySelector('button');
+    // const messageElement = encodeAndSendEl.querySelector('textarea');
+    // const decodeElement = decodeAndReadEl.querySelector('textarea');
+
+    const encodeButton = document.getElementsByTagName('button')[0];
+    const decodeButton =  document.getElementsByTagName('button')[1];
+    const messageElement =  document.getElementsByTagName('textarea')[0];
+    const decodeElement = document.getElementsByTagName('textarea')[1];
+
+    const encodeMessage = function(){
+        let result='';
+        let messageToEncode = messageElement.value;
+        Array.from(messageToEncode).forEach(letter=>result += String.fromCharCode(letter.charCodeAt(0)+1));
+        decodeElement.value = result;
+        messageElement.value = '';
+    };
+    const decodeMessage = function(){
+        let message = decodeElement.value;
+        let decodedMessage = '';
+        Array.from(message).forEach(let=>decodedMessage += String.fromCharCode(let.charCodeAt(0)-1));
+        decodeElement.value = decodedMessage;
+    };
+    encodeButton.addEventListener('click', encodeMessage);
+    decodeButton.addEventListener('click', decodeMessage);
 }
