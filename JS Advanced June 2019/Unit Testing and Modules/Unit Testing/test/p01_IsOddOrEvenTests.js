4c14f89d2625edca33b82a9ac10d95c21bcb40c0
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-06-30 16:47:11 +0300

    Add unit tests folder initial state

---
+++ JS Advanced June 2019/Unit Testing and Modules/Unit Testing/test/p01_IsOddOrEvenTests.js
@@ -0,0 +1,8 @@
+const {expect} = require('chai');
+const {isOddOrEven} = require('../.vscode/Even Or Odd/isOddOrEven');
+
+describe('EvenOddTest', ()=>{
+    it('test1',()=>{
+        expect(1).to.be.equal(1);
+    })
+})
