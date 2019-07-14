0b2917565f8d19afbe34e31a038225de503a1818
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-07-14 15:30:04 +0300

    Add p01_Bus-stop solution

--- JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/01.BUS-STOP/app.js
+++ JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/01.BUS-STOP/app.js
@@ -1,3 +1,41 @@
 function getInfo() {
-    console.log("TODO...");
+    const busStopId = document.getElementById('stopId');
+    const stopNameElement = document.getElementById('stopName');
+    const busesListElement = document.getElementById('buses');
+
+    const parseData = ({name, buses}) => {
+        return {name: name, buses: buses}
+    };
+    const addToDom = ({name, buses}) => {
+        stopNameElement.textContent = name;
+        for(const bus in buses) {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+            busesListElement.appendChild(liElement);
+        }
+
+    };
+    const loadData = function(data) {
+        busesListElement.innerHTML = '';
+        let parsedData = parseData(data);
+        addToDom(parsedData);
+        busStopId.value = '';
+    }
+
+    const displayError = function() {
+        stopNameElement.textContent = 'Error';
+    }
+
+    fetch(`https://judgetests.firebaseio.com/businfo/${busStopId.value}.json `)
+        .then((response)=> {
+            if(response.ok){
+                return response.json();
+            }else{
+                displayError();
+                return;
+            }
+            
+        })
+        .then((data)=>loadData(data))
+        .catch(()=>displayError());
 }
