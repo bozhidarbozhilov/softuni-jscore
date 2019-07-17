9797325a6307c901d64e5cc8a2138363448e9872
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-07-17 08:38:06 +0300

    Add p02. Bus Schedule solution

--- JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/02.BUS-SCHEDULE/app.js
+++ JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,43 @@
 function solve() {
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+    const infoElement = document.querySelector('#info span');
+    let isStopped = true;
+    let stopName = '';
+    let nextId = 'depot';
+
+    const toggleButtons = ()=>{
+        if(isStopped){
+            departButton.disabled = 'true';
+            arriveButton.disabled = '';
+            isStopped = false;
+        }else{
+            departButton.disabled = '';
+            arriveButton.disabled = 'true';
+            isStopped = true;
+        }
+    }
 
     function depart() {
-        console.log('Depart TODO...');
+        let url = (`https://judgetests.firebaseio.com/schedule/${nextId}.json`);
+        fetch(url)
+            .then((response)=>response.json())
+            .then((data)=>{
+                stopName = data.name;
+                nextId = data.next;
+                infoElement.textContent = `Next stop ${stopName}`;
+                toggleButtons();
+            })
+            .catch((err)=> {
+                infoElement.textContent = 'Error';
+                departButton.disabled = 'true';
+                arriveButton.disabled = 'true';
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoElement.textContent = `Arriving at ${stopName}`;
+        toggleButtons();
     }
 
     return {
