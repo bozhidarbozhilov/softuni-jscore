56a1c31042b45ae15cdd0aae0440168cc15957c3
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-07-17 17:14:36 +0300

    Add p04 Messenger Solution

--- JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/04.MESSENGER/app.js
+++ JS Applications July 2019/JS-Applications-REST-Services-and-AJAX-Exercise/04.MESSENGER/app.js
@@ -1,5 +1,41 @@
 function attachEvents() {
-    console.log('TODO...');
+    const textArea = document.getElementById('messages');
+    const authorInput = document.getElementById('author');
+    const contentInput = document.getElementById('content');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    const url = 'https://rest-messanger.firebaseio.com/messanger.json';
+
+    const uploadMessage = function() {
+        let authorName = authorInput.value;
+        let messageContent = contentInput.value;
+
+        let message = {author:authorName, 
+            content:messageContent}
+
+        fetch(url, {
+            method: 'post',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(message)
+        })
+
+        authorInput.value = '';
+        contentInput.value= '';
+    }
+
+    const loadAllMessages = function() {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data)=>{
+                let values = Object.values(data);
+                values.forEach((message)=>{
+                    textArea.textContent += `${message.author}: ${message.content}\n`
+                })
+            });
+    }
+    submitBtn.addEventListener('click', uploadMessage);
+    refreshBtn.addEventListener('click', loadAllMessages);
 }
 
 attachEvents();
