dd304bc8fc41c3445c4e2e7b02ab9e8b61e77f91
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-08-10 16:56:19 +0300

    Add HTTP Status Cats Solution

--- JS Applications July 2019/JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
+++ JS Applications July 2019/JS-Applications-Templating-Exercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,35 @@
 (() => {
-     renderCatTemplate();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    renderCatTemplate();
+
+
+    function renderCatTemplate() {
+        const templateString = document.getElementById('cat-template').innerHTML;
+        const catsSection = document.getElementById('allCats');
+        const cats = window.cats;
+
+        const templateFunc = Handlebars.compile(templateString);
+
+        let html = templateFunc({ cats });
+
+        catsSection.innerHTML = html;
+
+        const handleStatusCode = ({ target })=> {
+            if(target.classList.contains('showBtn')){
+                const statusDiv = target.parentNode.querySelector('.status');
+                if(statusDiv.style.display === 'none') {
+                    statusDiv.style.display = '';
+                    target.textContent = 'Hide status code';
+                }else{
+                    statusDiv.style.display = 'none';
+                    target.textContent = 'Show status code';
+                }
+                    
+            }
+        }
+        catsSection.addEventListener('click', handleStatusCode)
+    }
+
+
+
+})()
