0266442b72bb0c5cd2dbbbeb26aa603da22fd089
bozhidarbozhilov <bozhidarbozhilov@gmail.com>
2019-08-11 19:32:53 +0300

    Add Popular Monkey Solution

--- JS Applications July 2019/JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ JS Applications July 2019/JS-Applications-Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,21 @@
 $(() => {
-    // TODO
+    const template = document.getElementById('monkey-template').innerHTML;
+
+    const templateFunc = Handlebars.compile(template);
+
+    document.body.innerHTML = templateFunc({monkeys});
+
+    let monkeysDiv = document.querySelector('.monkeys');
+
+    monkeysDiv.addEventListener('click', toggleMonkeyInfo);
+
+    function toggleMonkeyInfo({ target }) {
+        if(target.textContent === "Info") {
+            let infoParagraph = target.parentNode.querySelector('p');
+            infoParagraph.style.display === 'none' ?
+                infoParagraph.style.display = '' :
+                infoParagraph.style.display = 'none';   
+        }
+    }
+
 })
